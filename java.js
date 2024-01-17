db75a82fac9ade3fbf55bc9d7b7494328ebf9847
Marwan <mtm.1234elmasry@gmail.com>
2024-01-17 15:51:26 +0200

    counter app

---
+++ java.js
@@ -0,0 +1,14 @@
+let count=0 
+let saveEl=document.getElementById("save-el")
+let countEl=document.getElementById("count-el")
+
+function increase(){
+    count += 1
+    countEl.textContent=count
+}
+function save(){
+    let countStr = count + " - "
+    saveEl.textContent +=countStr
+    countEl.textContent=0
+    count=0
+}
